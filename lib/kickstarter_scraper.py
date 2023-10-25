ea5cd8e207f856e8c99fe2a09daff923f5834bab
RegisLewis <regisaslewis@gmail.com>
2023-10-24 18:59:35 -0700

    scraping kickstarter

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,12 +1,7 @@
 from bs4 import BeautifulSoup
 import ipdb
 
-# projects: kickstarter.select("li.project.grid_4")[0]
-# title: project.select("h2.bbcard_name strong a")[0].text
-# image link: project.select("div.project-thumbnail a img")[0]['src']
-# description: project.select("p.bbcard_blurb")[0].text
-# location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+
 
 def create_project_dict():
     html = ''
@@ -14,18 +9,21 @@ def create_project_dict():
         html = file.read()
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
+
+
     # Iterate through the projects
-    for project in kickstarter.select("li.project.grid_4"):
-        title = project.select("h2.bbcard_name strong a")[0].text
+    for n in kickstarter.select("li.project.grid_4"):
+        title = n.select("h2.bbcard_name strong a")[0].text
         projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+            "image_link": n.select("div.project-thumbnail a img")[0]["src"],
+            "description": n.select("p.bbcard_blurb")[0].text,
+            "location": n.select("li span.location-name")[0].text,
+            "percent_funded": n.select("li.first.funded")[0].text.replace("\n", "").replace("%", "% ")
         }
     # return the projects dictionary
 
     return projects
 
 projects = create_project_dict()
-print(projects)
+for n in projects.values():
+    print(n["percent_funded"])
